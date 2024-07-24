c306b486f5e903841bf4959943ba223b238c05a6
Andrii <wins00k@gmail.com>
2024-07-24 16:23:28 +0300

    dz-12 done

--- dz-12/src/test/java/org/tests/ManWomenTest.java
+++ dz-12/src/test/java/org/tests/ManWomenTest.java
@@ -1,5 +1,6 @@
 package org.tests;
 
+import dataproviders.TestDataProvider;
 import org.application.Man;
 import org.application.Women;
 import org.testng.Assert;
@@ -7,17 +8,22 @@ import org.testng.annotations.Test;
 
 public class ManWomenTest {
 
-    Man man = new Man("Kyrylo","Danilenko",67,false);
-    Women women = new Women("Antonina","Vasylenko",68,false);
 
-    @Test
-    public void isRetiredTest() {
+    @Test(dataProvider = "manData", dataProviderClass = TestDataProvider .class)
+    public void isRetiredManTest(String firstname, String lastname, int age, boolean partner) {
+        Man man = new Man(firstname,lastname,age,partner);
         Assert.assertEquals(man.isRetired(),true);
-        Assert.assertEquals(women.isRetired(),true);
+    };
+    @Test(dataProvider = "womenData", dataProviderClass = TestDataProvider.class)
+    public void isRetiredWomenTest(String firstname, String lastname, int age, boolean partner) {
+        Women women = new Women(firstname,lastname,age,partner);
+        Assert.assertEquals(women.isRetired(),false);
     };
 
-    @Test
-    public void registerPartnerShipTest() {
+    @Test(dataProvider = "womenData", dataProviderClass = TestDataProvider.class)
+    public void registerPartnerShipWomenTest(String firstname, String lastname, int age, boolean partner) {
+        Man man = new Man("Vasya", "Danilenko", 67, false);
+        Women women = new Women(firstname,lastname,age,partner);
         women.registerPartnership(man);
         Assert.assertEquals(women.getLastName(),"Danilenko");
         Assert.assertEquals(women.getPartner(),true);
